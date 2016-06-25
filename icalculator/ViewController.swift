5d92d999a633d2e98e5a0163b85a126960943ef8
18404715032 <20141105066yh@admindeiMac-36.local>
2016-06-25 11:59:09 +0800

    第二次
    将加减乘除和clear加入，运算方法编出，但是还未解决显示的问题，点完第一个数后无法清空，再点第二个数会加在原来的数上

--- icalculator/ViewController.swift
+++ icalculator/ViewController.swift
@@ -5,28 +5,46 @@
 //  缓存cache
 //  Created by 杨浩 on 16/6/22.
 //  Copyright © 2016年 杨浩. All rights reserved.
-//
+//  Display 显示缓存
 
 import UIKit
 
 class ViewController: UIViewController {
 
     var Display = ""
-    var cache = 0.0
+ //   var cache = 0.0
+    var one:Double=0.0
+    var two:Double=0.0
+    var b_fuzhi:Bool=false
+    var b_state:Bool=false
+    var b_dian:Bool=false
+    var result:Double=0.0
+    
+    
     @IBOutlet weak var x: UITextField!
     @IBOutlet weak var y: UITextField!
     @IBOutlet weak var z: UITextField!
     @IBAction func add(sender: AnyObject) {
+        ys("add")
     }
     @IBAction func reduce(sender: AnyObject) {
+        ys("reduce")
     }
     @IBAction func ride(sender: AnyObject) {
+        ys("ride")
     }
     @IBAction func except(sender: AnyObject) {
+        ys("except")
     }
     @IBAction func clear(sender: AnyObject) {
-        Display = ""
-        self.x.text = ""
+        result=0.0
+        one=0.0
+        two=0.0
+        Display=""
+        b_fuzhi=false
+        b_state=false
+        b_dian=false
+        x.text="0.0"
     }
     @IBAction func h1(sender: AnyObject) {
         click("1")
@@ -63,18 +81,49 @@ class ViewController: UIViewController {
     }
 
     @IBAction func equal(sender: AnyObject) {
+        if(b_state==true){
+            two=0
+        }
+        evaluation()
+        if(fuhao=="add"){
+            result=one+two        }
+        else if(fuhao=="reduce"){
+            result=one-two
+        }
+        else if(fuhao=="ride"){
+            result=one*two        }
+        else if(fuhao=="except"){
+            result=one/two
+        }
+        else{
+            result=one
+        }
+        b_fuzhi=false
+        b_state=false
+        x.text="\(result)"
     }
     func click (number:String){
         Display += number
         self.x.text = Display
     }
     
-    func evaluation(){
-        if !Display.isEmpty{
-            let P = Display as NSString
-            cache = P.doubleValue
-            Display = ""
+    
+    func evaluation/*qiuzhi*/(){
+        if(b_fuzhi==false){
+            one=(x.text! as NSString).doubleValue
+            b_fuzhi=true
         }
+        else{
+            two=(x.text! as NSString).doubleValue
+            b_fuzhi=true
+        }
+        
+    }
+    var fuhao:String=""
+    func ys(fu:String){
+        evaluation()
+        fuhao=fu
+        b_state=false
     }
     override func viewDidLoad() {
         super.viewDidLoad()
